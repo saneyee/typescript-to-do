6497667769b962c1e9bf33220404dcf7e863ed20
Saneyee Sarkar <Guest@epicodus-103.local>
2017-12-04 16:08:39 -0800

    typescript todo list

---
+++ app/to-do.ts
@@ -0,0 +1,21 @@
+class Task {
+  done: boolean = false;
+
+
+  constructor(public description: string, public priority: string){}
+
+  markDone(){
+    this.done = true;
+  }
+}
+
+var tasks: Task[] = [];
+tasks.push(new Task('Do the dishes.', 'Medium'));
+tasks.push(new Task('Buy chocolate.', 'Low'));
+tasks.push(new Task('Do laundry.', 'High'));
+
+tasks[0].markDone();
+
+for(var task of tasks){
+  console.log(task);
+}
